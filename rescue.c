5d45279f1971cbb9c3ad132f0fa82679d5cbc847
pmpnethsara <nethpawan77@gmail.com>
2026-05-31 23:50:01 +0530

    complete structure

--- rescue.c
+++ rescue.c
@@ -12,9 +12,11 @@ struct rescueTeam{
     char teamName[CHARMAX];
     int memberCount;
     char vehicleType[CHARMAX];
-    int teamX;
+    int teamX;     // Location track from coordinates
     int teamY;
     int availability;
+	int totalMissions;
+    int completedMissions;
 };
 
 struct rescueMissions{
@@ -27,9 +29,13 @@ struct rescueMissions{
 
 };
 
+void rescueTeamMenu(struct rescueTeam team[], struct rescueMissions mission[], int *count, int *count2);
+
 void addRescueteam(struct rescueTeam team[], int *count ); 
 void viewRescueteam(struct rescueTeam team[], int count);
-int assignRescueteam(struct rescueMissions mission[], struct rescueTeam team[], int *count22, int *count); 
+int assignRescueteam(struct rescueMissions mission[], struct rescueTeam team[], int *count2, int *count); 
+void trackMission(struct rescueTeam team[], struct rescueMissions mission[], int *count2);
+void performanceReport(struct rescueTeam team[], int count); 
 
 char buffer[CHARMAX];
 
@@ -42,15 +48,15 @@ int main () {
 
     do{
 
-        printf("\n================= RESCUE COORDINATION SYSTEM ==================\n");
+        printf("\n================= RESCUE COORDINATION SYSTEM ==================\n\n");
         printf("1. victim registry \n");
         printf("2. Rescue Teams \n");
         printf("3. Supply Management \n");
         printf("4. Shelter Camps \n");
         printf("5. Incident Reports \n");
-        printf("6. Exit \n\n");
+        printf("6. Exit \n");
 
-        printf("Enter Your Choise : ");
+        printf("\nEnter Your Choise : ");
         fgets(buffer, CHARMAX, stdin);
         buffer[strcspn(buffer, "\n")] = '\0';
         choice = atoi(buffer);
@@ -63,53 +69,8 @@ int main () {
                 break;
 
             case 2 :
-                do{
-                   
-
-                    printf("1. Add Reascue Team : \n");
-                    printf("2. View Reascue Team : \n");
-                    printf("3. Assign Rescue Teams : \n");
-                    printf("4. Track Rescue Missions : \n");
-
-                    printf("Enter Your Choice : ");
-                    fgets(buffer, CHARMAX, stdin);
-                    buffer[strcspn(buffer, "\n")] = '\0';
-                    int choice2 = atoi(buffer);
-
-                    switch(choice2) {
-
-                        case 1 :
-
-                            printf("How many teams Do you Want to Add : ");
-                            fgets(buffer, CHARMAX, stdin);
-                            buffer[strcspn(buffer, "\n")] = '\0';
-                            int  n = atoi(buffer);
-
-                            for(int i = 0; i < n ; i++) {
-
-                                addRescueteam(team, &count);
-
-                            }
-                            break;   
-
-                        case 2 : 
-
-                            viewRescueteam(team, count);              
-                            break;
-                        
-                        case 3 :
-                            assignRescueteam(mission, team, &count2, &count);
-                            break;
-
-                        case 4 :
-                            trackMission(team, mission);
-
-                    }
-
-
-
-                }while(choice != 7);
                 
+                rescueTeamMenu(team, mission, &count, &count2);
                 break;
 
             case 3 :
@@ -143,6 +104,71 @@ int main () {
 
 }
 
+void rescueTeamMenu(struct rescueTeam team[], struct rescueMissions mission[], int *count,  int *count2){
+	
+	int choice2;
+	do{
+                   
+
+        printf("1. Add Reascue Team \n");
+        printf("2. View Reascue Team \n");
+		printf("3. Assign Rescue Teams \n");
+        printf("4. Track Rescue Missions \n");
+		printf("5. Performance Report \n");
+
+        printf("\nEnter Your Choice : ");
+        fgets(buffer, CHARMAX, stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+        choice2 = atoi(buffer);
+
+        switch(choice2) {
+
+            case 1 :
+
+                printf("How many teams Do you Want to Add : ");
+                fgets(buffer, CHARMAX, stdin);
+                buffer[strcspn(buffer, "\n")] = '\0';
+                int  n = atoi(buffer);
+
+                for(int i = 0; i < n ; i++) {
+
+                    addRescueteam(team, count);
+
+                }
+                break;   
+
+            case 2 : 
+
+				viewRescueteam(team, *count);              
+                break;
+                        
+            case 3 :
+                assignRescueteam(mission, team, count2, count);
+                break;
+
+            case 4 :
+                trackMission(team, mission, count2);
+				break;
+
+            case 5 :
+				performanceReport(team, *count);
+				break;
+			
+			case 6 :
+			
+				break;
+				
+			default :
+			
+			
+				break;
+				
+		}
+
+	}while(choice2 != 7);
+		
+}
+
 void addRescueteam(struct rescueTeam team[], int *count ) {
     
     team[*count].teamID = *count;
@@ -171,6 +197,8 @@ void addRescueteam(struct rescueTeam team[], int *count ) {
     team[*count].teamY = atoi(buffer);
     
     team[*count].availability = 1;
+	team[*count].totalMissions = 0;
+    team[*count].completedMissions = 0;
 
     (*count)++;
      
@@ -197,16 +225,11 @@ void viewRescueteam(struct rescueTeam team[], int count) {
 
 int assignRescueteam(struct rescueMissions mission[], struct rescueTeam team[], int *count2, int *count) {
     
-    mission[count].missionID = *count2;
+    mission[*count2].missionID = *count2;
 
     printf("Enter Mission Type (Search,Rescue,Evacuation) : ");
     fgets(mission[*count2].rescueMission, CHARMAX, stdin);
     mission[*count2].rescueMission[strcspn(mission[*count2].rescueMission, "\n")] = '\0';
-    
-   /* printf("Enter Priority Level (1-5) : ");
-    fgets(buffer, CHARMAX, stdin);
-    buffer[strcspn(buffer, "\n")] = '\0';
-    mission[*count2].proirityLevel = atoi(buffer);*/
 
     printf("Enter Victim X coordinates : ");
     fgets(buffer, CHARMAX, stdin);
@@ -254,7 +277,7 @@ int assignRescueteam(struct rescueMissions mission[], struct rescueTeam team[], 
 }
 
 
-void trackMission(struct reascueTeam team[], struct rescueMissions mission[] ){
+void trackMission(struct rescueTeam team[], struct rescueMissions mission[], int *count2){
 
     printf("=========Current Rescue Mission Details==========\n");
 
@@ -265,15 +288,68 @@ void trackMission(struct reascueTeam team[], struct rescueMissions mission[] ){
         printf("Team ID = %d \n", mission[i].teamID);
         printf("Mission State = %s \n", mission[i].state ? "Finished" : "Ongoing");
     }
-    
-    
-        
 
+    int id;
+
+    do{
+
+        printf("Enter Finished Mission ID (-1 for Exit) : ");
+
+        fgets(buffer, CHARMAX, stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+        id = atoi(buffer);
+
+        mission[id].state = 1;
+        team[mission[id].teamID].availability = 1;
+        team[mission[id].teamID].completedMissions ++;
+
+    }while(id != -1);
+
+}
+
+void performanceReport(struct rescueTeam team[], int count) {
+
+
+	if(count == 0) {
+        printf("No rescue teams registered.\n");
+        return;
+    }
+	
+	int bestTeam = 0;
+	
+	for(int i = 0; i < count; i++) {
+
+        float successRate = 0;
+
+        if(team[i].totalMissions > 0) {
+            successRate = ((float)team[i].completedMissions / team[i].totalMissions) * 100;
+        }
+		
+		printf("Team ID = %d\n", team[i].teamID);
+        printf("Team Name = %s\n", team[i].teamName);
+        printf("Total Missions = %d\n", team[i].totalMissions);
+        printf("Completed Missions = %d\n", team[i].completedMissions);
+        printf("Success Rate = %.2f%%\n", successRate);
+
+		if(team[i].completedMissions > team[bestTeam].completedMissions) {
+
+            bestTeam = i;
+        }
+    }
+
+    printf("===== BEST PERFORMING TEAM =====\n");
+    printf("Team Name = %s\n", team[bestTeam].teamName);
+    printf("Completed Missions = %d\n", team[bestTeam].completedMissions);
+    
+    for(int i = 0; i < count ; i ++) {
+
+            printf("%s \t|", team[i].teamName);
+            for(int j = 0; j < team[i].completedMissions; j++){
+                printf("*");
+            }
+            printf("\n");
 
     }
 
-
 }
 
-
-}
